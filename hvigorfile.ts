0a6d19177e141d5d47430216cf9ff3371e13d80f
xuldqi <novemeber11@gmail.com>
2025-10-19 23:25:55 +0800

    修复构建配置：统一SDK 5.0.0(12)、modelVersion 6.0.0、清理hvigor文件格式

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,13 +1,6 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-  system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
-};
-
-import { appTasks } from '@ohos/hvigor-ohos-plugin';
-
-export default {
-  system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
-};
+    system: appTasks,
+    plugins:[]
+}
